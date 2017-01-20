452aec4ef19f33495ec25dc5672593af3f12fa4a
Ali Hassan <alithephantom@gmail.com>
2017-01-21 01:02:03 +0200

    add mouse event manager

--- source/Fan.hx
+++ source/Fan.hx
@@ -3,20 +3,31 @@ package;
 import flixel.FlxSprite;
 import flixel.FlxG;
 
+
 class Fan extends FlxSprite
 {
     private var fan:FlxSprite;
     private var sign:FlxSprite;
+    
+    public var rowIndex:Int;
+    public var colIndex:Int;
+
     public function new (x:Int, y:Int, rowIndex:Int, colIndex:Int)
     {
         super(x, y);
         loadGraphic("assets/images/stand.png");
 
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+        
         fan = new FlxSprite(x,y,"assets/images/fan.png");
         sign = new FlxSprite(x,y,"assets/images/sign.png");
         
         FlxG.state.add(this);
         FlxG.state.add(fan);
         FlxG.state.add(sign);
+
+
+		
     }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,6 +6,7 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.input.mouse.FlxMouseEventManager;
 
 class PlayState extends FlxState
 {
@@ -29,8 +30,16 @@ class PlayState extends FlxState
 			{
 				fans[colIndex].push(new Fan(offsetX + colIndex*verticalSapcing, offsetY + rowIndex*horizontalSapcing,
 									rowIndex, colIndex));
+				FlxMouseEventManager.add(fans[colIndex][rowIndex], onDown, null, null, null);
 			}
 		}
+	}
+
+
+    public function onDown(sprite:FlxSprite)
+	{
+        var fan:Fan = cast sprite;
+        trace("clicked @", fan.colIndex, fan.rowIndex);
 	}
 
 	override public function update(elapsed:Float):Void
