43330919d93702446b3c188b7759ad131f127f2d
Ali Hassan <alithephantom@gmail.com>
2017-01-22 15:41:03 +0200

    add Info State

--- source/InfoState.hx
+++ source/InfoState.hx
@@ -9,21 +9,25 @@ import flixel.math.FlxMath;
 
 import flixel.util.FlxColor;
 
-class MenuState extends FlxState
+class InfoState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
 		add(new FlxSprite(0,0,"assets/images/mainMinue.png"));
+
+		
+		
 	}
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
 		if(FlxG.keys.justPressed.W)
 		{
 			FlxG.camera.fade(FlxColor.BLACK, .33, false, function()
 			{
-				FlxG.switchState(new InfoState());
+				FlxG.switchState(new PlayState());
 			});
 		}
 	}
